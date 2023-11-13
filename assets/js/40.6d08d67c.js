3958a8e17cfc5ea3175c965370452ef2a67a22d8
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2023-11-13 16:02:22 +0100

    add erc20 list and bump version

--- assets/js/40.6d08d67c.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[40],{511:function(n,t,e){"use strict";e.r(t);var u=e(1),l=Object(u.a)({},(function(){return(0,this._self._c)("Content")}),[],!1,null,null,null);t.default=l.exports}}]);
