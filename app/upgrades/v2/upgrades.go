b4fab4f586c16f872613a36d02863e279499e27a
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2023-12-15 09:50:27 +0100

    add ica module registration to upgrades

--- app/upgrades/v2/upgrades.go
+++ app/upgrades/v2/upgrades.go
@@ -1,9 +1,19 @@
 package v2
 
 import (
+	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	ica "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts"
+	genesistypes "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/genesis/types"
+	icahosttypes "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/host/types"
+	icatypes "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/types"
+	transfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
 	"github.com/planq-network/planq/app/keepers"
 	"github.com/planq-network/planq/app/upgrades"
 )
@@ -16,6 +26,41 @@ func CreateUpgradeHandler(
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		logger := ctx.Logger().With("upgrade", UpgradeName)
+
+		// create ICS27 Controller submodule params, with the controller module NOT enabled
+		gs := &genesistypes.GenesisState{
+			ControllerGenesisState: genesistypes.ControllerGenesisState{},
+			HostGenesisState: genesistypes.HostGenesisState{
+				Port: icatypes.HostPortID,
+				Params: icahosttypes.Params{
+					HostEnabled: true,
+					AllowMessages: []string{
+						sdk.MsgTypeURL(&banktypes.MsgSend{}),
+						sdk.MsgTypeURL(&banktypes.MsgMultiSend{}),
+						sdk.MsgTypeURL(&distrtypes.MsgSetWithdrawAddress{}),
+						sdk.MsgTypeURL(&distrtypes.MsgWithdrawDelegatorReward{}),
+						sdk.MsgTypeURL(&govtypes.MsgVote{}),
+						sdk.MsgTypeURL(&govtypes.MsgVoteWeighted{}),
+						sdk.MsgTypeURL(&stakingtypes.MsgDelegate{}),
+						sdk.MsgTypeURL(&stakingtypes.MsgUndelegate{}),
+						sdk.MsgTypeURL(&stakingtypes.MsgCancelUnbondingDelegation{}),
+						sdk.MsgTypeURL(&stakingtypes.MsgBeginRedelegate{}),
+						sdk.MsgTypeURL(&transfertypes.MsgTransfer{}),
+					},
+				},
+			},
+		}
+
+		bz, err := icatypes.ModuleCdc.MarshalJSON(gs)
+		if err != nil {
+			return nil, errorsmod.Wrapf(err, "failed to marshal %s genesis state", icatypes.ModuleName)
+		}
+
+		// Register the consensus version in the version map to avoid the SDK from triggering the default
+		// InitGenesis function.
+		vm[icatypes.ModuleName] = ica.AppModule{}.ConsensusVersion()
+
+		_ = mm.Modules[icatypes.ModuleName].InitGenesis(ctx, icatypes.ModuleCdc, bz)
 
 		// Leave modules are as-is to avoid running InitGenesis.
 		logger.Debug("running module migrations ...")
