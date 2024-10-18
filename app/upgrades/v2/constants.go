f5308dd773fe0b6a2b184261dea00f3e6e83be1e
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2024-10-18 10:47:20 +0200

    revert back upgrade name

--- app/upgrades/v2/constants.go
+++ app/upgrades/v2/constants.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	// UpgradeName is the shared upgrade plan name for mainnet
-	UpgradeName = "v2.0.3"
+	UpgradeName = "v2.0.0"
 	// UpgradeInfo defines the binaries that will be used for the upgrade
 	UpgradeInfo = `''`
 
