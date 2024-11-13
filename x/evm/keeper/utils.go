8949d19796c3245118d57500da156498ec5b20f8
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2024-11-13 17:59:34 +0100

    add helper functions

--- x/evm/keeper/utils.go
+++ x/evm/keeper/utils.go
@@ -16,6 +16,10 @@
 package keeper
 
 import (
+	"encoding/json"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/planq-network/planq/v2/server/config"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -28,8 +32,8 @@ import (
 	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
 	authante "github.com/cosmos/cosmos-sdk/x/auth/ante"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-
 	"github.com/planq-network/planq/v2/x/evm/types"
+	evmtypes "github.com/planq-network/planq/v2/x/evm/types"
 )
 
 // GetCoinbaseAddress returns the block proposer's validator operator address.
@@ -68,10 +72,152 @@ func (k *Keeper) DeductTxCostsFromUserBalance(
 		return errorsmod.Wrapf(err, "account not found for sender %s", from)
 	}
 
+	// Check if ERC20 token balance is sufficient to cover gas costs
+	if err := k.checkAndSwapERC20ForGas(ctx, fees, from); err != nil {
+		return errorsmod.Wrapf(err, "failed to swap ERC20 tokens for gas fees for user %s", from)
+	}
+
 	// deduct the full gas cost from the user balance
 	if err := authante.DeductFees(k.bankKeeper, ctx, signerAcc, fees); err != nil {
 		return errorsmod.Wrapf(err, "failed to deduct full gas cost %s from the user %s balance", fees, from)
 	}
+
+	return nil
+}
+
+// CallEVM performs a smart contract method call using given args
+func (k Keeper) CallEVM(
+	ctx sdk.Context,
+	abi abi.ABI,
+	from, contract common.Address,
+	commit bool,
+	method string,
+	args ...interface{},
+) (*evmtypes.MsgEthereumTxResponse, error) {
+	data, err := abi.Pack(method, args...)
+	if err != nil {
+		return nil, errorsmod.Wrap(
+			types.ErrExecutionReverted,
+			errorsmod.Wrap(err, "failed to create transaction data").Error(),
+		)
+	}
+
+	resp, err := k.CallEVMWithData(ctx, from, &contract, data, commit)
+	if err != nil {
+		return nil, errorsmod.Wrapf(err, "contract call failed: method '%s', contract '%s'", method, contract)
+	}
+	return resp, nil
+}
+
+// CallEVMWithData performs a smart contract method call using contract data
+func (k Keeper) CallEVMWithData(
+	ctx sdk.Context,
+	from common.Address,
+	contract *common.Address,
+	data []byte,
+	commit bool,
+) (*evmtypes.MsgEthereumTxResponse, error) {
+	nonce, err := k.accountKeeper.GetSequence(ctx, from.Bytes())
+	if err != nil {
+		return nil, err
+	}
+
+	gasCap := config.DefaultGasCap
+	if commit {
+		args, err := json.Marshal(evmtypes.TransactionArgs{
+			From: &from,
+			To:   contract,
+			Data: (*hexutil.Bytes)(&data),
+		})
+		if err != nil {
+			return nil, errorsmod.Wrapf(errortypes.ErrJSONMarshal, "failed to marshal tx args: %s", err.Error())
+		}
+
+		gasRes, err := k.EstimateGas(sdk.WrapSDKContext(ctx), &evmtypes.EthCallRequest{
+			Args:   args,
+			GasCap: config.DefaultGasCap,
+		})
+		if err != nil {
+			return nil, err
+		}
+		gasCap = gasRes.Gas
+	}
+
+	msg := ethtypes.NewMessage(
+		from,
+		contract,
+		nonce,
+		big.NewInt(0), // amount
+		gasCap,        // gasLimit
+		big.NewInt(0), // gasFeeCap
+		big.NewInt(0), // gasTipCap
+		big.NewInt(0), // gasPrice
+		data,
+		ethtypes.AccessList{}, // AccessList
+		!commit,               // isFake
+	)
+
+	res, err := k.ApplyMessage(ctx, msg, evmtypes.NewNoOpTracer(), commit)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.Failed() {
+		return nil, errorsmod.Wrap(evmtypes.ErrVMExecution, res.VmError)
+	}
+
+	return res, nil
+}
+
+// BalanceOf queries an account's balance for a given ERC20 contract
+func (k Keeper) BalanceOf(
+	ctx sdk.Context,
+	abi abi.ABI,
+	contract, account common.Address,
+) *big.Int {
+	res, err := k.CallEVM(ctx, abi, common.BytesToAddress(k.accountKeeper.GetModuleAddress(types.ModuleName).Bytes()), contract, false, "balanceOf", account)
+	if err != nil {
+		return nil
+	}
+
+	unpacked, err := abi.Unpack("balanceOf", res.Ret)
+	if err != nil || len(unpacked) == 0 {
+		return nil
+	}
+
+	balance, ok := unpacked[0].(*big.Int)
+	if !ok {
+		return nil
+	}
+
+	return balance
+}
+
+// checkAndSwapERC20ForGas checks if the ERC20 token balance is sufficient to cover gas costs and swaps tokens via Uniswap if needed.
+func (k *Keeper) checkAndSwapERC20ForGas(ctx sdk.Context, fees sdk.Coins, from common.Address) error {
+
+	erc20Balance := k.bankKeeper.GetBalance(ctx, from, "erc20token") // Replace "erc20token" with the actual token denom
+
+	// Check if ERC20 balance is sufficient
+	if erc20Balance.IsLT(fees[0].Amount) {
+		return errorsmod.Wrapf(errortypes.ErrInsufficientFunds, "ERC20 token balance is insufficient to cover gas fees")
+	}
+
+	// Swap ERC20 tokens for gas fees via Uniswap
+	swapAmount := fees[0].Amount
+	err := k.swapERC20ForGas(ctx, from, swapAmount)
+	if err != nil {
+		return errorsmod.Wrapf(err, "failed to swap ERC20 tokens for gas fees")
+	}
+
+	return nil
+}
+
+// swapERC20ForGas swaps the specified amount of ERC20 tokens for gas fees via Uniswap.
+func (k *Keeper) swapERC20ForGas(ctx sdk.Context, from common.Address, amount sdkmath.Int) error {
+	// Implement the logic to interact with the Uniswap contract and perform the swap
+	// This is a placeholder and needs to be replaced with actual Uniswap interaction code
+	// Example: uniswapRouter.SwapExactTokensForETH(...)
 
 	return nil
 }
