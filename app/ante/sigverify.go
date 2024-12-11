d23d50288fa1fabf4e661b396da868dbd34cdc18
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2024-12-11 12:29:19 +0100

    allow ed25519 sigs

--- app/ante/sigverify.go
+++ app/ante/sigverify.go
@@ -60,7 +60,7 @@ func SigVerificationGasConsumer(
 	case *ed25519.PubKey:
 		// Validator keys
 		meter.ConsumeGas(params.SigVerifyCostED25519, "ante verify: ed25519")
-		return errorsmod.Wrap(errortypes.ErrInvalidPubKey, "ED25519 public keys are unsupported")
+		return nil
 
 	case multisig.PubKey:
 		// Multisig keys
